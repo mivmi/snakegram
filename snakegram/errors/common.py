3308f4ab989d2717c60652b1a729d219f5bc1bb8
unknown <mr.milad.heidari@gmail.com>
2025-07-12 08:49:48 -0700

    feat(error): add security error class

--- snakegram/errors/common.py
+++ snakegram/errors/common.py
@@ -5,3 +5,7 @@ class BaseError(Exception):
     This class serves as a foundation for defining other error types.
     """
     pass
+
+class SecurityError(BaseError):
+    """Raised when a security violation is detected."""
+    pass

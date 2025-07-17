9acd207b897a39800c8e4f55fe82f18e8dc9f0b5
unknown <mr.milad.heidari@gmail.com>
2025-07-17 12:26:34 -0700

    add new type aliases

--- snakegram/alias.py
+++ snakegram/alias.py
@@ -4,3 +4,7 @@ URL = t.NewType('URL', str)
 Host = t.NewType('Host', str)
 NetAddr: t.TypeAlias = t.Tuple[Host, int]
 Address: t.TypeAlias = t.Union[URL, NetAddr]
+
+Phone = t.NewType('Phone', str)
+Token = t.NewType('Token', str)
+PhoneOrToken: t.TypeAlias = t.Union[Phone, Token]

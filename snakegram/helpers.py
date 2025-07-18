e57c2853e565814a219c557132edb624771b844b
unknown <mr.milad.heidari@gmail.com>
2025-07-18 12:38:37 -0700

    add update helper

--- snakegram/helpers.py
+++ snakegram/helpers.py
@@ -63,3 +63,31 @@ def get_display_name(obj: t.Union[types.User, types.TypeChat]):
         result.append(getattr(obj, 'title', None))
 
     return ''.join(filter(None, result))
+
+
+def update_order_key(update) -> int:
+    """get sort key for the update based on `pts`/`qts` for ordering."""
+
+    pts = getattr(update, 'pts', None)
+    if pts is not None:
+        return pts - getattr(update, 'pts_count', 0)
+
+    qts = getattr(update, 'qts', None)
+    if qts is not None:
+        return qts - 1
+
+    return 0
+
+def get_update_channel_id(update: types.update.TypeUpdate) -> t.Optional[int]:
+    """get `channel_id` from a `types.update.TypeUpdate`, if available."""
+
+    channel_id = getattr(update, 'channel_id', None)
+    if channel_id is not None:
+        return channel_id
+    
+    # from message
+    message = getattr(update, 'message', None)
+    if message:
+        peer_id = getattr(message, 'peer_id', None)
+        if isinstance(peer_id, types.PeerChannel):
+            return peer_id.channel_id

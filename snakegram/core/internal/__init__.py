56677ae5c6414810c385a8336125e5fa1a2ab532
unknown <mr.milad.heidari@gmail.com>
2025-07-18 12:42:34 -0700

    add UpdateState for managing updates

---
+++ snakegram/core/internal/__init__.py
@@ -0,0 +1,9 @@
+from .update_state import UpdateState
+from .event_handler import EventHandler
+from .cache_entities import CacheEntities
+
+__all__ = [
+    'UpdateState',
+    'EventHandler',
+    'CacheEntities'
+]

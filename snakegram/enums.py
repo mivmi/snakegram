c14d7eb1ba8e42fa184b97f013e75b13141b4647
unknown <mr.milad.heidari@gmail.com>
2025-07-18 12:31:12 -0700

    add entity type enum

--- snakegram/enums.py
+++ snakegram/enums.py
@@ -16,3 +16,38 @@ class Operation(enum.Enum):
     @property
     def is_logical(self):
         return self in (Operation.OR, Operation.AND, Operation.NOT)
+
+
+class EntityType(enum.IntEnum):
+    BOT = enum.auto()
+    USER = enum.auto()
+    GROUP = enum.auto()
+    CHANNEL = enum.auto()
+    MEGAGROUP = enum.auto()
+    GIGAGROUP = enum.auto()
+
+    @property
+    def char(self):
+        return self.name[0]
+
+    @property
+    def is_user(self):
+        return self in (EntityType.BOT, EntityType.USER)
+    
+    @property
+    def is_group(self):
+        return self is EntityType.GROUP
+
+    @property
+    def is_channel(self):
+        return not any((self.is_user, self.is_group))
+
+    @classmethod
+    def from_char(cls, char: str):
+        if len(char) == 1:
+            char = char.upper()
+            for etype in EntityType:
+                if etype.char == char:
+                    return etype
+
+        raise ValueError(f'invalid entity type char: {char!r}')

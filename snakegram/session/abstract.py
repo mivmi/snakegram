e1ffef52a7eb729c3c1e89997fc7614e986362f1
unknown <mr.milad.heidari@gmail.com>
2025-07-18 12:37:28 -0700

    add state management and entity storage

--- snakegram/session/abstract.py
+++ snakegram/session/abstract.py
@@ -2,13 +2,21 @@ import typing as t
 from abc import ABC, abstractmethod
 
 if t.TYPE_CHECKING:
+    from ..models import Entity
     from ..crypto import AuthKey
 
+DEFAULT_STATE_DATE = 2 ** 31 - 1
 
 class AbstractSession(ABC):
     @abstractmethod
-    def clear(self):
+    def clear(self) -> None:
         """clears all session data."""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def me(self) -> t.Optional['Entity']:
+        """the current user's entity, if available."""
         raise NotImplementedError
 
     @property
@@ -38,12 +46,13 @@ class AbstractSession(ABC):
         return 0
 
     @abstractmethod
-    def set_dc(self, dc_id: int):
+    def set_dc(self, dc_id: int) -> None:
         """set the `dc_id` for this session."""
         raise NotImplementedError
 
+
     @abstractmethod
-    def set_auth_key(self, auth_key: bytes, created_at: int):
+    def set_auth_key(self, auth_key: bytes, created_at: int) -> None:
         """
         set the auth key.
 
@@ -54,7 +63,7 @@ class AbstractSession(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def set_time_offset(self, time_offset: int):
+    def set_time_offset(self, time_offset: int) -> None:
         """
         set time offset between the client's local time and the server's time
         """
@@ -66,7 +75,7 @@ class AbstractSession(ABC):
         salt: int,
         valid_since: int,
         valid_until: int,
-    ):
+    ) -> None:
         """adds server salt to valid salts."""
         raise NotImplementedError
 
@@ -91,6 +100,49 @@ class AbstractSession(ABC):
         """
         raise NotImplementedError
 
+    # entity
+    @abstractmethod
+    def get_entity(
+        self,
+        *,
+        id: int = None,
+        username: str = None,
+        phone_number: str = None
+    ) -> t.Optional['Entity']:
+        """get entity by `id`, `username`, or `phone_number`."""
+        pass
+
+    @abstractmethod
+    def upsert_entity(self, entity: 'Entity') -> None: 
+        """inserts or updates entity."""
+        pass
+    
+    # state
+    @abstractmethod
+    def set_state(
+        self,
+        pts: int,
+        qts: int,
+        seq: int,
+        date: int
+    ) -> None:
+        """set state values."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_state(self) -> t.Tuple[int, int, int, int]:
+        """get state as (pts, qts, seq, date)."""
+        raise NotImplementedError
+    
+    @abstractmethod
+    def get_channel_pts(self, id: int) -> int:
+        """get `pts` for channel by its `id`."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_channel_pts(self, id: int, pts: int) -> None:
+        """set `pts` for channel by its `id`."""
+        raise NotImplementedError
 
 class AbstractPfsSession(ABC):
     @abstractmethod
@@ -122,7 +174,7 @@ class AbstractPfsSession(ABC):
         auth_key: bytes,
         created_at: int,
         expires_at: int
-    ):
+    ) -> None:
         """set the auth key."""
         raise NotImplementedError
 
@@ -132,7 +184,7 @@ class AbstractPfsSession(ABC):
         salt: int,
         valid_since: int,
         valid_until: int,
-    ):
+    ) -> None:
         """adds server salt to valid salts."""
         raise NotImplementedError
 
--- snakegram/session/sqlite_session.py
+++ snakegram/session/sqlite_session.py
@@ -6,10 +6,12 @@ import typing_extensions as te
 
 from functools import wraps
 
-
 from ..about import __version__, __update_command__
+from ..enums import EntityType
+from ..models import Entity
+
 from ..crypto import AuthKey
-from .abstract import AbstractSession, AbstractPfsSession
+from .abstract import DEFAULT_STATE_DATE, AbstractSession, AbstractPfsSession
 from ..network.datacenter import DEFAULT_DC_ID
 
 
@@ -257,8 +259,40 @@ class SqliteSession(BaseSqlite, AbstractSession):
             created_at='INTEGER',
             time_offset='INTEGER DEFAULT 0',
         )
+        
+        # state
+        self._create_table(
+            cursor,
+            'state',
+            id='INTEGER PRIMARY KEY CHECK (id = 1)',
+            pts='INTEGER',
+            qts='INTEGER',
+            seq='INTEGER',
+            date='INTEGER DEFAULT 0'
+        )
 
-        # server salts
+        # entities
+        self._create_table(
+            cursor,
+            'entities',
+            id='INTEGER PRIMARY KEY',
+            type='TEXT',
+            access_hash='INTEGER',
+            name='TEXT',
+            is_self='TINYINT DEFAULT 0',
+            username='TEXT',
+            phone_number='TEXT',
+            pts='INTEGER',
+            modified_at='DATETIME DEFAULT CURRENT_TIMESTAMP'
+        )
+
+        # index's
+        cursor.execute(
+            '''
+            CREATE INDEX IF NOT EXISTS `entities_is_self` ON `entities` (`is_self`)
+            '''
+        )
+
         super()._create(cursor)
         self._load_session(cursor)
 
@@ -266,6 +300,13 @@ class SqliteSession(BaseSqlite, AbstractSession):
         self._dc_id: int = DEFAULT_DC_ID
         self._created_at: int = 0
         self._time_offset: int = 0
+
+        #
+        self._pts: int = 0
+        self._qts: int = 0
+        self._seq: int = 0
+        self._state_date: int = DEFAULT_STATE_DATE
+        self._entity: t.Optional[Entity] = None
 
         cursor.execute(
             '''
@@ -286,26 +327,60 @@ class SqliteSession(BaseSqlite, AbstractSession):
 
             if session[1] is not None:
                 self._auth_key.set_auth_key(session[1])
+
+        # state
+        cursor.execute(
+            '''
+            SELECT
+                `pts`,
+                `qts`,
+                `seq`,
+                `date`
+            FROM `state` WHERE id = 1
+            '''
+        )
+        state = cursor.fetchone()
+        if state:
+            self._pts = state[0]
+            self._qts = state[1]
+            self._seq = state[2]
+            self._state_date = state[3]
         
-        else:
-            cursor.execute(
-                '''
-                INSERT INTO `session`
-                (
-                    `dc_id`,
-                    `auth_key`,
-                    `created_at`,
-                    `time_offset`
-                )
-                VALUES (?, ?, ?, ?)
-                ''',
-                (
-                    self._dc_id,
-                    self._auth_key.key,
-                    self._created_at,
-                    self._time_offset
-                )
+        # self-entities
+        cursor.execute(
+            '''
+            SELECT 
+                `id`,
+                `type`,
+                `access_hash`,
+                `name`,
+                `username`,
+                `phone_number`
+            FROM `entities` WHERE `is_self` = 1
+            '''
+        )
+        
+        entity = cursor.fetchone()
+        if entity:
+            entity_type = (
+                EntityType.from_char(entity[1])
+                if entity[1] else 
+                None  
             )
+            self._entity = Entity(
+                entity[0],
+                entity_type,
+                entity[2],
+
+                name=entity[3],
+                is_self=True,
+                username=entity[4],
+                phone_number=entity[5]
+            )
+
+    @property
+    def me(self):
+        return self._entity
 
     @property
     def dc_id(self):
@@ -322,7 +397,14 @@ class SqliteSession(BaseSqlite, AbstractSession):
 
         cursor.execute(
             '''
-            UPDATE `session` SET `dc_id` = ? WHERE `id` = 1;
+            INSERT INTO `session` 
+            (
+                `id`,
+                `dc_id`
+            )
+            VALUES (1, ?)
+            ON CONFLICT(`id`) DO UPDATE SET 
+                dc_id = EXCLUDED.dc_id
             ''',
             (dc_id,)
         )
@@ -331,15 +413,18 @@ class SqliteSession(BaseSqlite, AbstractSession):
     def set_auth_key(self, cursor, auth_key, created_at):
         cursor.execute(
             '''
-            UPDATE `session` SET
-                `auth_key` = ?,
-                `created_at` = ?
-            WHERE id = 1;
-            ''',
+            INSERT INTO `session` 
             (
-                auth_key,
-                created_at
+                `id`,
+                `auth_key`,
+                `created_at`
             )
+            VALUES (1, ?, ?)
+            ON CONFLICT(`id`) DO UPDATE SET 
+                auth_key = EXCLUDED.auth_key,
+                created_at = EXCLUDED.created_at
+            ''',
+            (auth_key, created_at)
         )
         super().set_auth_key(cursor, auth_key, created_at)
     
@@ -350,15 +435,197 @@ class SqliteSession(BaseSqlite, AbstractSession):
         time_offset: int
     ):
         self._time_offset = time_offset
-
         cursor.execute(
             '''
-            UPDATE `session` SET
-                `time_offset` = ?
-            WHERE id = 1;
+            INSERT INTO `session` 
+            (
+                `id`,
+                `time_offset`
+            )
+            VALUES (1, ?)
+            ON CONFLICT(`id`) DO UPDATE SET 
+                time_offset = EXCLUDED.time_offset
             ''',
             (time_offset,)
         )
+    
+    # entities
+    @with_cursor
+    def get_entity(
+        self,
+        cursor: sqlite3.Cursor,
+        *,
+        id: int = None,
+        username: str = None,
+        phone_number: str = None
+
+    ):
+
+        wheres = {}
+        if id is not None:
+            wheres['id'] = id
+
+        if username is not None:
+            wheres['username'] = username
+
+        if phone_number is not None:
+            wheres['phone_number'] = phone_number
+
+        if not wheres:
+            raise ValueError(
+                'WHERE clause is empty (no id, username, or phone_number given).'
+            )
+
+        where_sql = ' OR '.join(f'`{k}` = ?' for k in wheres)
+        cursor.execute(
+            f'''
+            SELECT 
+                `id`,
+                `type`,
+                `access_hash`,
+                `is_self`,
+                `name`,
+                `username`,
+                `phone_number`
+            FROM `entities` WHERE {where_sql}
+                LIMIT 1
+  
+            ''',
+            tuple(wheres.values())
+        )
+
+        result = cursor.fetchone()
+        if result is not None:
+            entity_type = (
+                EntityType.from_char(result[1])
+                if result[1] else 
+                None  
+            )
+
+            return Entity(
+                result[0],
+                entity_type,
+                result[2],
+                name=result[3],
+                is_self=result[4],
+                username=result[5],
+                phone_number=result[6]
+            )
+
+    @with_cursor
+    def upsert_entity(self, cursor: sqlite3.Cursor, entity: Entity): 
+        cursor.execute(
+            '''
+            INSERT INTO `entities` 
+                (
+                    `id`,
+                    `type`,
+                    `access_hash`,
+                    `is_self`,
+                    `name`,
+                    `username`,
+                    `phone_number`
+                )
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+            ON CONFLICT(`id`) DO UPDATE SET 
+                `type` = COALESCE(EXCLUDED.type, `type`),
+                `access_hash` = COALESCE(EXCLUDED.access_hash, `access_hash`),
+                `is_self` = COALESCE(EXCLUDED.is_self, `is_self`),
+                `name` = COALESCE(EXCLUDED.name, `name`),
+                `username` = COALESCE(EXCLUDED.username, `username`),
+                `phone_number` = COALESCE(EXCLUDED.phone_number, `phone_number`),
+                `pts` = COALESCE(EXCLUDED.pts, `pts`),
+                `modified_at` = CURRENT_TIMESTAMP
+
+            ''',
+            (
+                entity.id,
+                entity.type.char if entity.type else None,
+                entity.access_hash,
+                (
+                    (1 if entity.is_self else 0)
+                    if entity.is_self is not None else
+                    None
+                ),
+                entity.name,
+                entity.username,
+                entity.phone_number
+            )
+        )
+
+        if entity.is_self:
+            self._entity = entity
+
+    # update state
+    @with_cursor
+    def set_state(
+        self,
+        cursor: sqlite3.Cursor,
+        pts: int = None,
+        qts: int = None,
+        seq: int = None,
+        date: int = None
+    ):
+        self._pts = pts or self._pts
+        self._qts = qts or self._qts
+        self._seq = seq or self._seq
+        self._state_date = date or self._state_date
+
+        cursor.execute(
+            '''
+            INSERT INTO `state` 
+            (
+                `id`,
+                `pts`,
+                `qts`,
+                `seq`,
+                `date`
+            )
+            VALUES (1, ?, ?, ?, ?)
+            ON CONFLICT(`id`) DO UPDATE SET 
+                `pts` = EXCLUDED.pts,
+                `qts` = EXCLUDED.qts,
+                `seq` = EXCLUDED.seq,
+                `date` = EXCLUDED.date
+            
+            ''',
+            (self._pts, self._qts, self._seq, self._state_date)
+        )
+
+    def get_state(self):
+        date = self._state_date or DEFAULT_STATE_DATE
+        return self._pts, self._qts, self._seq, date
+
+    @with_cursor
+    def get_channel_pts(self, cursor: sqlite3.Cursor, id: int) -> int:
+        cursor.execute(
+            '''
+            SELECT 
+                `pts`
+            FROM `entities` WHERE `id` = ?
+            ''',
+            (id,)
+        )
+        result = cursor.fetchone()
+        return result[0] if result else 0
+
+    @with_cursor
+    def set_channel_pts(self, cursor: sqlite3.Cursor, id: int, pts: int):
+        cursor.execute(
+            '''
+            INSERT INTO `entities` 
+                (
+                    `id`,
+                    `pts`  
+                )
+            VALUES (?, ?)
+            ON CONFLICT(`id`) DO UPDATE SET 
+                `pts` = COALESCE(EXCLUDED.pts, `pts`),
+                `modified_at` = CURRENT_TIMESTAMP
+            ''',
+            (id, pts)
+        )
+
 
 class SqlitePfsSession(BaseSqlite, AbstractPfsSession):
     def _create(self, cursor: sqlite3.Cursor):
@@ -396,24 +663,6 @@ class SqlitePfsSession(BaseSqlite, AbstractPfsSession):
             if session[0] is not None:
                 self._auth_key.set_auth_key(session[0])
         
-        else:
-            cursor.execute(
-                '''
-                INSERT INTO `session`
-                (
-                    `auth_key`,
-                    `created_at`,
-                    `expires_at`
-                )
-                VALUES (?, ?, ?)
-                ''',
-                (
-                    self._auth_key.key,
-                    self._created_at,
-                    self._expires_at
-                )
-            )
-
 
     @property
     def expires_at(self):
@@ -423,17 +672,21 @@ class SqlitePfsSession(BaseSqlite, AbstractPfsSession):
     def set_auth_key(self, cursor, auth_key, created_at, expires_at):
         cursor.execute(
             '''
-            UPDATE `session` SET
-                `auth_key` = ?,
-                `created_at` = ?,
-                `expires_at` = ?
-            WHERE `id` = 1;
-            ''',
+            INSERT INTO `session` 
             (
-                auth_key,
-                created_at,
-                expires_at
+                `id`,
+                `auth_key`,
+                `created_at`,
+                `expires_at`
+                
             )
+            VALUES (1, ?, ?, ?)
+            ON CONFLICT(`id`) DO UPDATE SET 
+                auth_key = EXCLUDED.auth_key,
+                created_at = EXCLUDED.created_at,
+                expires_at = EXCLUDED.expires_at
+            ''',
+            (auth_key, created_at, expires_at)
         )
         self._expires_at = expires_at
         super().set_auth_key(cursor, auth_key, created_at)

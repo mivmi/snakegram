4d7526a9fb4866063777fa6dee590a0c600d444a
milad <61160040+mivmi@users.noreply.github.com>
2025-08-31 05:13:39 -0700

    switch build system from maturin to setuptools-rust

--- backend.py
+++
@@ -1,9 +0,0 @@
-from maturin import *
-from builder import generate_code
-
-
-_get_requires_for_build_wheel = get_requires_for_build_wheel
-
-def get_requires_for_build_wheel(config_settings=None):
-    generate_code()
-    return _get_requires_for_build_wheel(config_settings)
--- snakegram/about.py
+++ snakegram/about.py
@@ -1,6 +1,6 @@
 from inspect import cleandoc as _cleandoc
 
-__version_info__ = (1, 0, 0, 'dev', 1)
+__version_info__ = (1, 0, 0, 'dev', 2)
 
 
 def _get_version(version_info: tuple) -> str:

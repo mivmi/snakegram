02b45f2bb9619264394a616200ef5437623b5c3f
milad <61160040+mivmi@users.noreply.github.com>
2025-09-20 06:58:50 -0700

    update aes wrapper for class based extension

--- snakegram/crypto/aes.py
+++ snakegram/crypto/aes.py
@@ -1,9 +1,26 @@
+import typing as t
 from .._rust import crypto
+ 
 
-def aes_ctr256(data: bytes, key: bytes, nonce: bytes) -> bytes:
+# backends
+if t.TYPE_CHECKING:
+    class Aes256Ctr:
+        def __init__(self, key: bytes, nonce: bytes): ...
+        def __call__(self, data: bytes) -> bytes: ...
+    
+    class Aes256Ige:
+        def __init__(self, key: bytes, iv: bytes): ...
+        def encrypt(self, plain_text: bytes, hash: bool=False) -> bytes: ...
+        def decrypt(self, cipher_text: bytes, hash: bool=False) -> bytes: ...
+
+
+Aes256Ctr = crypto.Aes256Ctr
+Aes256Ige = crypto.Aes256Ige
+
+
+def aes_ctr256(data: bytes, key: bytes, nonce: bytes):
     """Encrypts or decrypts data using `AES-CTR-256`."""
-    return crypto.aes_ctr256(data, key, nonce)
-
+    return Aes256Ctr(key, nonce)(data)
 
 def aes_ige256_encrypt(
     plain_text: bytes,
@@ -12,13 +29,7 @@ def aes_ige256_encrypt(
     hash: bool=False
 ) -> bytes:
     """Encrypts plain-text using `AES-IGE-256`."""
-
-    return crypto.aes_ige256_encrypt(
-        plain_text,
-        key,
-        iv,
-        hash
-    )
+    return Aes256Ige(key, iv).encrypt(plain_text, hash)
 
 def aes_ige256_decrypt(
     cipher_text: bytes,
@@ -27,10 +38,4 @@ def aes_ige256_decrypt(
     hash: bool=False
 ) -> bytes:
     """Decrypts cipher-text using `AES-IGE-256`."""
-    
-    return crypto.aes_ige256_decrypt(
-        cipher_text,
-        key,
-        iv,
-        hash
-    )
+    return Aes256Ige(key, iv).decrypt(cipher_text, hash)
